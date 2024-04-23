f96e3992495907ae18df739d0f1aa8b815268b9d
IjayAbby <inyangasi4@gmail.com>
2024-04-23 17:07:22 +0100

    Hero Section done

--- src/pages/landingPage/components/Hero.tsx
+++ src/pages/landingPage/components/Hero.tsx
@@ -1,11 +1,5 @@
-import {
-  Flex,
-  Spacer,
-  Text,
-  Button,
-  ButtonGroup,
-  Center,
-} from "@chakra-ui/react";
+import { Flex, Box, Text, Image, Heading } from "@chakra-ui/react";
+import heroImage from "../../../assets/heroImage.jpg";
 
 export default function Hero() {
   return (
@@ -16,20 +10,19 @@ export default function Hero() {
       justify={"space-between"}
       w={"100%"}
     >
-      <div>
-        <Center w="50%" bg="green.500">
-          <Text fontSize="3xl">Discover Top Creators And Influencers</Text>
-          <Text fontSize="2xl">
-            Jamii aim is to simplify the influencer marketing process for brands
-            and provide them with access to a diverse range of talented creators
-            who can help them achieve their marketing goals.
-          </Text>
-          <Button colorScheme="blue">Get Started</Button>
-        </Center>
-      </div>
-      <div>
-        <h2>Image Here</h2>
-      </div>
+      <Box flex="1">
+        <Heading as="h1" size="4xl" noOfLines={2} mb={6}>
+          Discover Top Creators And Influencers
+        </Heading>
+        <Text fontSize="2xl">
+          Jamii aim is to simplify the influencer marketing process for brands
+          and provide them with access to a diverse range of talented creators
+          who can help them achieve their marketing goals.
+        </Text>
+      </Box>
+      <Box flex="1">
+        <Image src={heroImage} alt="Description of the image" boxSize="90%" />
+      </Box>
     </Flex>
   );
 }
