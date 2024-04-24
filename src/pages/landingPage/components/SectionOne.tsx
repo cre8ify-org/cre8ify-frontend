5c98f96fb7f41c5b76bdeb60e331b32dd3e15a51
IjayAbby <inyangasi4@gmail.com>
2024-04-24 01:47:13 +0100

    Add section

---
+++ src/pages/landingPage/components/SectionOne.tsx
@@ -0,0 +1,28 @@
+import { Flex, Box, Text, Image, Heading } from "@chakra-ui/react";
+import heroImage from "../../../assets/heroImage.jpg";
+
+export default function SectionOne() {
+  return (
+    <Flex
+      gap={"4"}
+      p={"2rem"}
+      align={"center"}
+      justify={"space-between"}
+      w={"100%"}
+    >
+      <Box flex="1">
+        <Image src={heroImage} alt="Description of the image" boxSize="90%" />
+      </Box>
+      <Box flex="1">
+        <Heading as="h1" size="4xl" noOfLines={2} mb={6}>
+          Discover Top Creators And Influencers
+        </Heading>
+        <Text fontSize="2xl">
+          Jamii aim is to simplify the influencer marketing process for brands
+          and provide them with access to a diverse range of talented creators
+          who can help them achieve their marketing goals.
+        </Text>
+      </Box>
+    </Flex>
+  );
+}
--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SectionOne from "./components/SectionOne";
+import SectionTwo from "./components/SectionTwo";
+import Card from "./components/Card";
 function LandingPage() {
   return (
     <div>
       <Header />
       <Hero />
+      <SectionOne />
+      <SectionTwo />
+      <Card />
     </div>
   );
 }
