903f7528f195954b3a84fbdcfedbbe0340a761ed
Victoria <adedayovicky123@gmail.com>
2024-04-27 05:55:39 +0100

    CHORE: clean up code, make register logic a component

--- src/layout/dashboardLayout/index.tsx
+++ src/layout/dashboardLayout/index.tsx
@@ -6,35 +6,19 @@ import {
   Text,
   Modal,
   ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
   useDisclosure,
-  FormLabel,
-  Input,
-  FormControl,
 } from "@chakra-ui/react";
 import { menu } from "../../constants/data.ts";
 import { NavLink } from "react-router-dom";
-import { ChangeEvent, useState } from "react";
-import { FaImage } from "react-icons/fa6";
+import { useState } from "react";
 import "../../App.css";
 import ConnectButton from "../../components/ConnectButton.tsx";
-import useRegister from "../../hooks/useRegister.tsx";
-import { useWeb3ModalAccount } from "@web3modal/ethers/react";
-
+import { RegisterCreator } from "../../components/RegisterCreator.tsx";
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
 const DashboardLayout = (props: DashboardLayoutProps) => {
-  const [username, setUsername] = useState<string>("");
-  const [cid, setCid] = useState<string>("");
-
-  const { address } = useWeb3ModalAccount();
-
   const OverlayOne = () => (
     <ModalOverlay
       bg="blackAlpha.300"
@@ -44,52 +28,6 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [overlay, setOverlay] = useState(<OverlayOne />);
-
-  const handleRegister = useRegister(
-    username,
-    `${import.meta.env.VITE_GATEWAY_URL}/ipfs/${cid}`
-  );
-
-  const changeHandler = async (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const selectedFile = e.target.files[0];
-      await handleSubmission(selectedFile);
-    }
-  };
-
-  const handleSubmission = async (fileToUpload: string | Blob) => {
-    try {
-      const formData = new FormData();
-      formData.append("file", fileToUpload);
-      const metadata = JSON.stringify({
-        name: "File name",
-      });
-      formData.append("pinataMetadata", metadata);
-
-      const options = JSON.stringify({
-        cidVersion: 0,
-      });
-      formData.append("pinataOptions", options);
-
-      const res = await fetch(
-        "https://api.pinata.cloud/pinning/pinFileToIPFS",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${import.meta.env.VITE_PINATA_JWT}`,
-          },
-          body: formData,
-        }
-      );
-
-      const resData = await res.json();
-      setCid(resData.IpfsHash);
-      console.log(resData.IpfsHash);
-    } catch (e) {
-      console.log(e);
-      alert("Trouble uploading file");
-    }
-  };
 
   return (
     <Flex h="100vh">
@@ -160,90 +98,7 @@ const DashboardLayout = (props: DashboardLayoutProps) => {
       </Box>
       <Modal isCentered isOpen={isOpen} onClose={onClose}>
         {overlay}
-        <ModalContent bg={"#262628"} className="font">
-          <ModalHeader>Create your account</ModalHeader>
-          <ModalCloseButton
-            _focus={{ outline: "none" }}
-            _hover={{ border: "1px solid #15AB99" }}
-          />
-          <ModalBody pb={6}>
-            <Box>
-              <Input
-                type="file"
-                border={"none"}
-                id="selectFile"
-                onChange={changeHandler}
-                hidden
-              />
-              <Flex align={"end"} justify={"space-between"} mb={"1rem"}>
-                <label htmlFor="selectFile">
-                  <Flex
-                    borderRadius={".5rem"}
-                    align={"center"}
-                    justify={"center"}
-                    color={"#B7B7B6"}
-                    w={"200px"}
-                    h={"150px"}
-                    bg={"#323436"}
-                  >
-                    <Flex flexDirection={"column"} align={"center"}>
-                      <Icon as={FaImage} fontSize={"3rem"} />
-                      <Text fontSize={"1rem"}>Upload Profile</Text>
-                    </Flex>
-                  </Flex>
-                </label>
-              </Flex>
-            </Box>
-            <FormControl>
-              <FormLabel>Username</FormLabel>
-              <Input
-                placeholder="Username"
-                value={username}
-                _placeholder={{ color: "#767677" }}
-                size="md"
-                border={"1px solid #535354"}
-                outline={"none"}
-                _hover={{ outline: "none" }}
-                _focus={{ boxShadow: "none" }}
-                px={".5rem"}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <FormLabel>Address</FormLabel>
-              <Input
-                size="md"
-                border={"1px solid #535354"}
-                outline={"none"}
-                _hover={{ outline: "none" }}
-                _focus={{ boxShadow: "none" }}
-                px={".5rem"}
-                value={address}
-                disabled
-              />
-            </FormControl>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              bgGradient="linear(to-r, #04A67D, #24B1B6)"
-              borderRadius={"100rem"}
-              border={"none"}
-              color={"#fff"}
-              transition={"all .5s ease-in-out"}
-              w={"150px"}
-              _hover={{
-                bgGradient: "linear(to-r, #04A67D, #24B1B6)",
-                border: "none",
-              }}
-              _focus={{ outline: "none" }}
-              onClick={handleRegister}
-            >
-              <Text>Register</Text>
-            </Button>
-          </ModalFooter>
-        </ModalContent>
+        <RegisterCreator />
       </Modal>
     </Flex>
   );
