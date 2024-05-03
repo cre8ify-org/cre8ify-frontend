3144afe17a394d6c2e23a69340c8e8ddb4273b36
IjayAbby <inyangasi4@gmail.com>
2024-05-03 05:05:21 +0100

    Table Layout

--- src/pages/DAOdashboard/Proposals/components/PTable.tsx
+++ src/pages/DAOdashboard/Proposals/components/PTable.tsx
@@ -1,41 +1,90 @@
 import {
-  TableContainer,
   Table,
   Thead,
+  Tbody,
+  Tfoot,
   Tr,
   Th,
-  Tbody,
   Td,
+  TableCaption,
+  TableContainer,
+  Button,
+  HStack,
 } from "@chakra-ui/react";
+import { GoArrowUpRight } from "react-icons/go";
+import { LuThumbsUp, LuThumbsDown } from "react-icons/lu";
+
+let proposalData = [
+  {
+    proposal: "Market Place ",
+    status: "Ongoing button",
+    timeLeft: "25hrs 15mins",
+    totalVotes: "1000",
+    votesFor: "925",
+    votesAgainst: "25",
+    vote: "Thumbs up or down click",
+  },
+];
 
 const PTable = () => {
   return (
     <TableContainer>
-      <Table variant="striped" colorScheme="gray">
+      <Table variant="striped" colorScheme="">
+        {/* <TableCaption>Imperial to metric conversion factors</TableCaption> */}
         <Thead>
           <Tr>
-            <Th color="#fff">Proposal</Th>
-            <Th color="#fff">Status</Th>
-            <Th color="#fff">Time Left</Th>
+            <Th>Proposal</Th>
+            <Th>Status</Th>
+            <Th isNumeric>Time Left</Th>
+            <Th isNumeric>Total Votes</Th>
+            <Th isNumeric>Votes For</Th>
+            <Th isNumeric>Votes Against</Th>
+            <Th isNumeric>Vote</Th>
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td color="#000">Market Place</Td>
-            <Td color="#000">Ongoing</Td>
-            <Td color="#000">25.4</Td>
-          </Tr>
-          <Tr>
-            <Td color="#fff">Ongoing</Td>
-            <Td color="#fff">Successful</Td>
-            <Td color="#fff">30.48</Td>
-          </Tr>
-          <Tr>
-            <Td color="#000">yards</Td>
-            <Td color="#000">Cancelled</Td>
-            <Td color="#000">0.91444</Td>
-          </Tr>
+          {proposalData.map((item, index) => (
+            <Tr key={index}>
+              <Td>
+                {item.proposal}
+                <GoArrowUpRight />
+              </Td>
+              <Td>
+                {item.status === "Ongoing" ? (
+                  <Button color="primary">Ongoing</Button>
+                ) : (
+                  <Button color="secondary">Completed</Button>
+                )}
+              </Td>
+              <Td isNumeric>{item.timeLeft}</Td>
+              <Td isNumeric>{item.totalVotes}</Td>
+              <Td isNumeric>{item.votesFor}</Td>
+              <Td isNumeric>{item.votesAgainst}</Td>
+              <Td isNumeric>
+                <HStack spacing={2}>
+                  <LuThumbsUp
+                    style={{ marginRight: "10px" }}
+                    onClick={() => {
+                      /* Increase votes for */
+                    }}
+                  />
+                  <LuThumbsDown
+                    onClick={() => {
+                      /* Increase votes against */
+                    }}
+                  />
+                </HStack>
+              </Td>
+            </Tr>
+          ))}
         </Tbody>
+        {/* <Tfoot>
+          <Tr>
+            <Th>To convert</Th>
+            <Th>into</Th>
+            <Th isNumeric>multiply by</Th>
+          </Tr>
+        </Tfoot> */}
       </Table>
     </TableContainer>
   );
